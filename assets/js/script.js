266806eb0468df0f57fc532f5bddb18b5d340e5f
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2021-12-26 17:33:34 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,7 +130,7 @@ $(".list-group").on("change", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
-    // .trim();
+  // .trim();
 
   // get the parent ul's id attribute
   var status = $(this)
@@ -173,7 +173,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -211,15 +211,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event) {
     // console.log("activate", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function (event) {
     // console.log("deactivate", this);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function (event) {
     // console.log("over", event.target);
+    $(event.target).addClass("dropover-active")
   },
   out: function (event) {
     // console.log("out", event.target);
+    $(event.target).removeClass("dropover-active")
   },
   update: function (event) {
     // array to store the task data in 
@@ -267,9 +273,11 @@ $("#trash").droppable({
   },
   over: function (event, ui) {
     // console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function (event, ui) {
     // console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active")
   }
 });
 
@@ -300,9 +308,20 @@ var auditTask = function (taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  // check to see that the setInterval function is working
+  // console.log(taskEl)
 };
+
+// jQuery selector passes each element it finds using the selector into the callback function
+// that element is expressed in the el argument of the function. 
+// auditTask() then passes the element to its routines using the el argument
+// loop over every task on the page with a class of list-group-item and execute the auditTask() function to check the due date of each one. 
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  })
+}, 1800000) // 30 minutes
 
 // load tasks for the first time
 loadTasks();
-
-
